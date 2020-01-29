45ba7c74a8f0a5ea2890086003a9061598cbda52
banusadaiyappan <banusadaiyappan328@gmail.com>
2020-01-29 12:54:07 +0530

    Doggie_API

--- Models/Sensordata.js
+++ Models/Sensordata.js
@@ -3,9 +3,9 @@ var Schema = mongoose.Schema;
 var SensorSchema = new Schema(
     {
         temperaure: { type: String},
-        "x1-x10": { type: String },
-        "y1-y10": {type: String},
-        "z1-z10" :{type: String}
+        x: { type: String },
+        y: {type: String},
+        z :{type: String}
         
   
     });
--- routes/api.js
+++ routes/api.js
@@ -20,4 +20,28 @@ Router.get('/getdata', function (req, res) {
 
 });
 
+Router.post('/i/getdata', function (req, res) {
+	//res.send({ success: true});
+	//console.log(req.body)
+	
+	var tempdata = req.body;
+	
+	
+		Sensor.insertMany(tempdata,function (err, sensor) {
+			if (err) {
+				res.send({success:false,message:"error based on original error",error:err});
+			}
+			else{
+				res.send({success:true,message:"saved successfully",data:sensor});
+
+			}
+		});
+		
+		
+	
+	}
+   
+);
+
+
 module.exports = Router;
